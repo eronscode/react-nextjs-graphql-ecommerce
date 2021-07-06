0d9404a1b95e0046533497608c6c88b2bcf428b6
osemudiamen Elebesunu <eronscode@gmail.com>
2021-07-06 03:10:57 +0100

    implemented header component + global styles

---
+++ frontend/components/Header.js
@@ -0,0 +1,49 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
+
+function Header() {
+  return (
+    <HeaderStyles>
+      <div className="bar">
+        <Logo>
+          <Link href="/">Sick Fits</Link>
+        </Logo>
+      </div>
+      <div className="sub-bar">
+        <p>search</p>
+      </div>
+    </HeaderStyles>
+  );
+}
+
+export default Header;
