ec5fc3fed1de7f79e746324f12af9f3c4c622f0f
osemudiamen Elebesunu <eronscode@gmail.com>
2021-07-06 04:03:50 +0100

    Added some configutation for keystone.js

---
+++ backend/keystone.ts
@@ -0,0 +1,27 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+  },
+  lists: createSchema({}),
+  ui: {
+    isAccessAllowed: () => true,
+  },
+});
