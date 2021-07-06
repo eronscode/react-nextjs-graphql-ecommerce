bd53e51e4fbdccd7fb0e6e69d041abbd7097ccd8
osemudiamen Elebesunu <eronscode@gmail.com>
2021-07-06 04:29:01 +0100

    Added authentication to keystone

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,6 +1,12 @@
 import { config, createSchema } from '@keystone-next/keystone/schema';
-import { User } from './schemas/User';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 import 'dotenv/config';
+
+import { User } from './schemas/User';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -10,21 +16,43 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in inital roles here
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-  },
-  lists: createSchema({
-    User,
-  }),
-  ui: {
-    isAccessAllowed: () => true,
-  },
+  //   passwordResetLink: {
+  //     async sendToken(args) {
+  //       // send the email
+  //       await sendPasswordResetEmail(args.token, args.identity);
+  //     },
+  //   },
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+    },
+    lists: createSchema({
+      User,
+    }),
+    ui: {
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: 'id',
+    }),
+  })
+);
