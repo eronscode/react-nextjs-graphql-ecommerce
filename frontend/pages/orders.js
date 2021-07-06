c3ae24e59eee50fb7b2ac1aa6fcc5316b0fce69c
osemudiamen Elebesunu <eronscode@gmail.com>
2021-07-06 03:26:54 +0100

    Fixed styled components css warning on serverside rendering

---
+++ frontend/pages/orders.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function OrdersPage() {
+  return <p>Hello</p>;
+}
+
+export default OrdersPage;
---
+++ frontend/pages/products.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProductsPage() {
+  return <p>Hello</p>;
+}
+
+export default ProductsPage;
