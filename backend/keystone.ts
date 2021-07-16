48207f140fd198d058e365b18e6d1eb2d0cb74e8
eronscode <eronscode@gmail.com>
2021-07-16 15:12:08 +0100

    Added mail function to send mail for password reset

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,18 +1,18 @@
-import { config, createSchema } from '@keystone-next/keystone/schema';
-import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import { createAuth } from "@keystone-next/auth";
 import {
   withItemData,
   statelessSessions,
-} from '@keystone-next/keystone/session';
-import { ProductImage } from './schemas/ProductImage';
-import { Product } from './schemas/Product';
-import 'dotenv/config';
+} from "@keystone-next/keystone/session";
+import { ProductImage } from "./schemas/ProductImage";
+import { Product } from "./schemas/Product";
+import "dotenv/config";
 
-import { User } from './schemas/User';
-import { insertSeedData } from './seed-data';
+import { User } from "./schemas/User";
+import { insertSeedData } from "./seed-data";
 
 const databaseURL =
-  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
@@ -20,19 +20,19 @@ const sessionConfig = {
 };
 
 const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  secretField: 'password',
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
   initFirstItem: {
-    fields: ['name', 'email', 'password'],
+    fields: ["name", "email", "password"],
     // TODO: Add in inital roles here
   },
-  //   passwordResetLink: {
-  //     async sendToken(args) {
-  //       // send the email
-  //       await sendPasswordResetEmail(args.token, args.identity);
-  //     },
-  //   },
+  passwordResetLink: {
+    async sendToken(args) {
+      // send the email
+      // await sendPasswordResetEmail(args.token, args.identity);
+    },
+  },
 });
 
 export default withAuth(
@@ -44,11 +44,11 @@ export default withAuth(
       },
     },
     db: {
-      adapter: 'mongoose',
+      adapter: "mongoose",
       url: databaseURL,
       async onConnect(keystone) {
-        console.log('Connected to the database!');
-        if (process.argv.includes('--seed-data')) {
+        console.log("Connected to the database!");
+        if (process.argv.includes("--seed-data")) {
           await insertSeedData(keystone);
         }
       },
@@ -63,7 +63,7 @@ export default withAuth(
     },
     session: withItemData(statelessSessions(sessionConfig), {
       // GraphQL Query
-      User: 'id',
+      User: "id",
     }),
   })
 );
