0d917e604e840166073be1dcda7bd81ea691465a
osemudiamen Elebesunu <eronscode@gmail.com>
2021-07-16 02:29:12 +0100

    File restructuring

---
+++ frontend/lib/graphql/products.graphql.js
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
+
+export { CREATE_PRODUCT_MUTATION };
